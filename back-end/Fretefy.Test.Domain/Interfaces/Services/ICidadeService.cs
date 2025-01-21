73f74635749d4313e32ecff31e9b6d3e8852af7b
Arthur Martins Thomé <dev.arthurthome@gmail.com>
2025-01-21 00:06:54 -0300

    Teste

--- back-end/Fretefy.Test.Domain/Interfaces/Services/ICidadeService.cs
+++ back-end/Fretefy.Test.Domain/Interfaces/Services/ICidadeService.cs
@@ -1,4 +1,5 @@
 ﻿using Fretefy.Test.Domain.Entities;
+using Fretefy.Test.Domain.Entities.Auxiliar;
 using System;
 using System.Collections.Generic;
 
@@ -10,5 +11,6 @@ namespace Fretefy.Test.Domain.Interfaces
         IEnumerable<Cidade> List();
         IEnumerable<Cidade> ListByUf(string uf);
         IEnumerable<Cidade> Query(string terms);
+        DefaultReturn<Cidade> AdicionarCidade(Cidade cidade);
     }
 }
